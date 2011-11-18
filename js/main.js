7d3d249f6833aaf73756946fcf5ad65c3fa17ec3
Chris Gomez <cgomez@everylastpixel.com>
2011-11-18 01:07:31 -0500

    Redwork the transaction form; Add some control buttons; Setup dynamic workspace height

--- js/main.js
+++ js/main.js
@@ -1,5 +1,36 @@
+/**
+ * Purse
+ *
+ * A place to put money
+ **/
+var Purse = Backbone.Model.extend({
+
+});
+
+/**
+ * Transaction
+ *
+ * A transfer of money
+ **/
+var Transaction = Backbone.Model.extend({
+
+});
+
 $(document).ready(function() {
-
-
-
+    window.user = new Purse();
 })
+
+$(window).resize(_.debounce(function() {
+    var windowHeight = $(window).height();
+    var headerHeight = $('#header').outerHeight();
+    var footerHeight = $('#footer').outerHeight();
+    var workspaceHeight = windowHeight - (headerHeight + footerHeight) + 'px';
+
+    console.log(windowHeight, headerHeight, footerHeight, workspaceHeight);
+
+    //console.log(workspaceHeight); return;
+
+    $('#workspace').css({ height: workspaceHeight });
+
+    console.log($('#workspace').css('height'));
+}, 300));
